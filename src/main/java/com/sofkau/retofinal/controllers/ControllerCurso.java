7044853266a5e01beacec8b2f7e077ed6eefacae
nicovelar <nicovelar@gmail.com>
2022-09-12 16:16:18 -0300

    Cambiar de delete/put a POST

--- src/main/java/com/sofkau/retofinal/controllers/ControllerCurso.java
+++ src/main/java/com/sofkau/retofinal/controllers/ControllerCurso.java
@@ -28,22 +28,15 @@ public class ControllerCurso {
                 .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
     }
 
-    @GetMapping("/find/{id}")
-    Mono<ResponseEntity<CursoDto>> findById(@PathVariable("id") String id) {
-        return service.findById(id)
-                .flatMap(cursoDto -> Mono.just(ResponseEntity.ok(cursoDto)))
-                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
-    }
-
-    @PutMapping("/update/{id}")
+    @PostMapping("/update/{id}")
     Mono<ResponseEntity<CursoDto>> update(@RequestBody Curso curso, @PathVariable("id") String id) {
         return service.update(curso,id)
                 .flatMap(cursoDto -> Mono.just(ResponseEntity.ok(cursoDto)))
                 .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
     }
 
-    @DeleteMapping("/delete/{id}")
-    public Mono<Void> delete(@PathVariable("id") String id) {
+    @PostMapping("/delete")
+    public Mono<Void> delete(@RequestBody String id) {
         return  service.deleteById(id);
     }
 
