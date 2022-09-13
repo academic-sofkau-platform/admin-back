dc7750469ba5438cb8c03bf43d73118b325865a5
luchoca <luchocastro.3@gmail.com>
2022-09-13 11:45:15 -0300

    Se agrega Banco de Mejoras

--- src/main/java/com/sofkau/retofinal/services/DiagnosticoRendimientoServiceImpl.java
+++ src/main/java/com/sofkau/retofinal/services/DiagnosticoRendimientoServiceImpl.java
@@ -20,40 +20,42 @@ import java.util.List;
     @Service
     public class DiagnosticoRendimientoServiceImpl {   //   Se debe ejecutar cuando se realice extracción de notas
 
-        public static void main(String[] args){
-            List<String> accionDeMejora = new ArrayList<String>();
-            accionDeMejora.add(1,"Repaso de conceptos de fundamentos de DDD (link de documentación");
-            accionDeMejora.add(2,"Repaso de componente estrategico de DDD (link de documentación)");
-            accionDeMejora.add(3,"Repaso de conceptos de fundamentos de Funcional y reactiva (link de documentación");
-            accionDeMejora.add(4,"Repaso de conceptos de fundamentos de WebFlux (link de documentación");
-            accionDeMejora.add(5,"Repaso de conceptos de Funcional y reactiva (link de documentación");
+        public class AccionDeMejora{
+            public AccionDeMejora(String cursos, String test, String accion) {
+            }
 
+            public void main(String args[]){
+                AccionDeMejora accionDeMejora[]=new AccionDeMejora[0];
+                accionDeMejora[0]=new AccionDeMejora("DDD", "Test 1", "Repaso de conceptos de fundamentos de DDD (link de documentación)");
+                accionDeMejora[1]=new AccionDeMejora("DDD", "Test 2", "Repaso de conceptos de fundamentos de DDD (link de documentación)");
+                accionDeMejora[2]=new AccionDeMejora("Reactividad", "Test 1","Repaso Reactiva (link documentación)");
+                accionDeMejora[2]=new AccionDeMejora("Reactividad", "Quiz 3","Repaso Reactiva  (link documentación)");
+                accionDeMejora[3]=new AccionDeMejora("Funcional", "Quiz 1", "Repaso Funcional y reactiva (link documentación)");
+                accionDeMejora[4]=new AccionDeMejora("Funcional", "Test 2", "Repaso Funcional y reactiva (link documentación)");
+                accionDeMejora[5]=new AccionDeMejora("Introduccion Desarrollo", "Quiz 3", "Repaso Introduccion al Desarrollo (link documentación)");
+                accionDeMejora[6]=new AccionDeMejora("Introduccion Desarrollo", "Test 2", "Repaso Introduccion al Desarrollo (link documentación)");
+
+
+
+
+            }
         }
+
+
     }
+
 
 
 //
 //        CURSO: DDD, Test 1, ACCION:Repaso de conceptos de fundamentos de DDD (link de documentación)
 //        CRUSO: DDD, Test 2, ACCION:Repaso de componente estrategico de DDD (link de documentación)
 //        CURSO: Reactividad: ACCION:Test 1: Funcional y reactiva (link documentación)
-//        seria todo bajo al 75%
+//        seria  bajo al 75%
 
-//    @Autowired
-//    NotasRepository repository;
-//    @Autowired
-//    ControllerTraining training;
 
-    //todo obtener aprendices
-//        @Override
-//
-//
-//        @Override
-//        public Flux<Notas> findAllNota() {  //todo filtar por nota <75%
-//            return repository.findAllNota();
-//        }
+//todo filtar por nota <75%
 
-        //todo recorrer List de accion de mejoras para ver si esta repetida
-
+// todo recorrer List de accion de mejoras para ver si esta repetida
 
 
 //Todo  DETERMINAR si un aprendiz esta en bajo rendimiento
@@ -66,25 +68,3 @@ import java.util.List;
 
 
 //Todo  No puedo enviar una accion de mejora repetida, es decir debo CONSULTAR si el aprendiz no tiene la accion de mejora correspodiente
-
-
-//
-//
-//        DoR
-//
-//        - Banco de acciones de mejora
-
-//        - Los valores o promedios para determinar si un aprendiz tiene bajo rendimiento
-//
-
-//        Criterios de Aceptación:
-
-
-//
-//
-//        ~~El aprendiz debe tener una propiedad para poder sumarle la accion de mejora~~ HECHO
-//
-//
-//        Dependencia:
-//
-//        Extracción de notas y envio de correos
